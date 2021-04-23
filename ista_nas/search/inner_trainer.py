2c4d488fded4b0462cb9f2195e384bd3d15a14a2
lmm6895071@126.com <lmm6895071@126.com>
2021-04-23 14:33:31 +0800

    debug

--- ista_nas/search/inner_trainer.py
+++ ista_nas/search/inner_trainer.py
@@ -79,7 +79,7 @@ class InnerTrainer:
             top5 = AverageMeter()
             prec1, prec5 = accuracy(scores, target, topk=(1, 5))
             losses.update(loss.item(), n)
-            top1.update(prec1.item(), n
+            top1.update(prec1.item(), n)
             top5.update(prec5.item(), n)
             print("------ acc {},={}".format(_i,top1))
             Acc.append(top1)
