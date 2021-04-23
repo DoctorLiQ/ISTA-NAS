39b78c56b04b35db5de11e53451bcddb1064288e
lmm6895071@126.com <lmm6895071@126.com>
2021-04-23 14:10:24 +0800

    update

--- ista_nas/search/inner_trainer.py
+++ ista_nas/search/inner_trainer.py
@@ -76,7 +76,7 @@ class InnerTrainer:
             print("------ acc {},={}".format(_i,acc))
             Acc.append(acc)
         b_nesh_normal,b_nesh_reduce = nesh_step(Acc,b_normals_index,b_reduce_index)
-        self.model._arch_parameters = [b_nesh_normal,b_nesh_reduce]
+        self.model._arch_parameters = [nn.Parameter(b_nesh_normal),nn.Parameter(b_nesh_reduce)]
             
     def train_epoch(self, train_queue, valid_queue, epoch):
         losses = AverageMeter()
