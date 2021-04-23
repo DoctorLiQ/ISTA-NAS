24a9443e0901471223eee91020bb98473adb6579
lmm6895071@126.com <lmm6895071@126.com>
2021-04-23 12:08:08 +0800

    update

--- ista_nas/search/nesh.py
+++ ista_nas/search/nesh.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch.nn as nn
 
 
-def nesh_step(Acc,normal,reduce):
+def nesh_step(Acc,normal_index,reduce_index):
     normals = torch.Tensor([[0,5,0.4,0.1],[0.6,0.2,0.2],[0.3,0.5,0.2],[0.1,0.9,0.0]])
     reduces = torch.Tensor([[0,5,0.4,0.1],[0.6,0.2,0.2],[0.3,0.5,0.2],[0.1,0.9,0.0]])
-    retunr normals,reduces
+    return normals,reduces
